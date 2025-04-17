f5dbb83e0f858f1648171f2481c80d3bf4c7e72a
alima-webdev <alima.webdev@gmail.com>
2025-04-17 00:18:11 -0400

    Improvements

--- consts.py
+++ consts.py
@@ -1,10 +1,15 @@
+# Python imports
+import os
+
+# Anki imports
 from aqt import mw
 from aqt.theme import theme_manager
 from pathlib import Path
 
+# Globals
 global ADDON_NAME, ADDON_TITLE, ADDON_PATH, HOST, MEDIA_SERVER, DARK_MODE, PANEL_WIDTH, PATH_TO_HTML, PATH_TO_DATAFILE, CONFIG
 
-ADDON_NAME = "anki-card-viewer"
+ADDON_NAME = os.getcwd().split(os.sep)[-1]
 ADDON_TITLE = mw.addonManager.addon_meta(ADDON_NAME).provided_name
 ADDON_PATH = Path(__file__).parent.absolute().as_posix()
 
